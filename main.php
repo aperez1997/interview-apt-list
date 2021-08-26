72ed31172185ff7dc4a9897d19a446bd2342315c
Tony Perez <aperez1997@yahoo.com>
2021-08-26 11:27:27 -0700

    round 2

--- main.php
+++ main.php
@@ -7,12 +7,85 @@
  * it really needs to know who has (or hasn't) been assigned yet in order to properly choose a
  * good candidate
  */
+
+const MIN_SIZE = 3;
+const MAX_SIZE = 5;
+
+
+$rv = testCandidateAlgo();
+printf("Result %b\n", $rv);
+
 $teamMgr = TeamMgr::MakeRandomTeams(3);
 
 
+/**
+printf("employees:\n");
+print_r(json_encode($teamMgr->getEmployees()));
+printf("\ncandidates:\n");
 $candidates = getCandidates($teamMgr);
 print_r(json_encode($candidates));
 printf("\n");
+ */
+
+function testCandidateAlgo()
+{
+    $result = true;
+    for ($i = 2; $i < 5; $i++){
+        $teamMgr = TeamMgr::MakeRandomTeams($i);
+        $candidates = getCandidates($teamMgr);
+        $employeeCount = $teamMgr->getEmployees();
+        printf("Team %s => Candidates %s\n", json_encode($teamMgr->getTeams()), json_encode($candidates));
+        $rv = testGroupComp($candidates, count($employeeCount));
+        if (!$rv) {
+            printf("Group [%s] failed\n", json_encode($candidates));
+        }
+        $result &= $rv;
+    }
+
+    $result &= testRepeat();
+
+    return $result;
+}
+
+function testRepeat()
+{
+    $teamMgr = TeamMgr::MakeRandomTeams(3);
+    $candidates1 = getCandidates($teamMgr);
+    $candidates2 = getCandidates($teamMgr);
+
+    if ($candidates1 == $candidates2){
+        printf("Got the same result! not random\n");
+        return false;
+    }
+    return true;
+}
+
+function testGroupComp($candidates, $expectedTotal)
+{
+    $foundCount = 0;
+    $foundEmployees = [];
+    foreach ($candidates as $group){
+        $count = count($group);
+        if ($count < MIN_SIZE || $count > MAX_SIZE){
+            printf("Error: group size is {$count} for %s\n", json_encode($group));
+            return false;
+        }
+        foreach ($group as $employee){
+            if (array_key_exists($employee, $foundEmployees)){
+                printf("Employee %s has appeared more than once\n", $employee);
+                return false;
+            }
+            $foundEmployees[$employee] = 1;
+        }
+
+        $foundCount += $count;
+    }
+    if ($foundCount != $expectedTotal){
+        printf("Wanted count %s got %s\n", $expectedTotal, $foundCount);
+        return false;
+    }
+    return true;
+}
 
 /**
  * @param TeamMgr $teamMgr
@@ -20,26 +93,15 @@ printf("\n");
  */
 function getCandidates(TeamMgr $teamMgr)
 {
-    $minGroupSize = 3;
-    $maxGroupSize = 5;
+    $minGroupSize = MIN_SIZE;
+    $maxGroupSize = MAX_SIZE;
 
     $candidates = $teamMgr->getEmployees();
 
     $groups = [];
 
-    $numInCompany = count($candidates);
-    for ($i = 0; $i < $numInCompany; $i++) {
-        $group = [];
-        for ($j = 0; $j < $maxGroupSize; $j++) {
-            // $candidate = array_pop($candidates);
-            $candidate = $teamMgr->findCandidate($group);
-            if (empty($candidate)) {
-                break;
-            }
-            $group[] = $candidate;
-        }
-        $groups[] = $group;
-    }
+    // list($i, $candidates, $groups) = getGroupsDefault($candidates, $maxGroupSize, $groups);
+    $groups = getGroupsSmarter($teamMgr);
 
 // printf("Check for min group size\n");
     $lastGroupIndex = count($groups) - 1;
@@ -56,15 +118,67 @@ function getCandidates(TeamMgr $teamMgr)
     return $groups;
 }
 
+/**
+ * @param array $candidates
+ * @param $maxGroupSize
+ * @param array $groups
+ * @return array
+ */
+function getGroupsDefault(array $candidates, $maxGroupSize, array $groups)
+{
+    shuffle($candidates);
+    $numInCompany = count($candidates);
+    for ($i = 0; $i < $numInCompany; $i++) {
+        $group = [];
+        for ($j = 0; $j < $maxGroupSize; $j++) {
+            $candidate = array_pop($candidates);
+            // $candidate = $teamMgr->findCandidate($group);
+            if (empty($candidate)) {
+                break;
+            }
+            $group[] = $candidate;
+        }
+        if (!empty($group)){
+            $groups[] = $group;
+        }
+    }
+    return array($i, $candidates, $groups);
+}
+
+function getGroupsSmarter(TeamMgr $teamMgr)
+{
+    $groups = [];
+    $candidates = $teamMgr->getEmployees();
+    shuffle($candidates);
+    $numInCompany = count($candidates);
+    for ($i = 0; $i < $numInCompany; $i++) {
+        $group = [];
+        for ($j = 0; $j < MAX_SIZE; $j++) {
+            $candidate = $teamMgr->findCandidate($group, $candidates);
+            if (empty($candidate)) {
+                break;
+            }
+            $removeIdx = array_search($candidate, $candidates);
+            unset($candidates[$removeIdx]);
+            $group[] = $candidate;
+        }
+        if (!empty($group)){
+            $groups[] = $group;
+        }
+    }
+    return $groups;
+}
+
+
 class TeamMgr
 {
     public static function MakeRandomTeams($numTeams)
     {
-        $numInTeam = 5;
         $candidateIdx = 1;
 
         $teams = [];
         foreach (range(1, $numTeams) as $teamNum) {
+            $numInTeam = rand(3,6);
             $teamIds = range($candidateIdx, $candidateIdx + $numInTeam - 1);
 
             $team = new Team($teamIds);
@@ -105,7 +219,40 @@ class TeamMgr
         return $employees;
     }
 
-    public function findCandidate($currentGroup)
+    public function getTeamForEmployee($employee){
+        foreach ($this->teams as $team) {
+            if ($team->isOnTeam($employee)){
+                return $team;
+            }
+        }
+        return null;
+    }
+
+    public function findCandidate($currentGroup, $remainingCandidate)
+    {
+        // look at all remaining candidates
+        foreach ($remainingCandidate as $candidate){
+            // see if any of them are in the same team as a current group member
+            $hasTeamMemberAlready = false;
+            foreach ($currentGroup as $currentMember){
+                $team = $this->getTeamForEmployee($currentMember);
+
+                if ($team->isOnTeam($candidate)){
+                    $hasTeamMemberAlready = true;
+                    break;
+                }
+            }
+
+            if (!$hasTeamMemberAlready){
+                return $candidate;
+            }
+        }
+
+        // what if we didnt find someone: return the first remaining candidate
+        return reset($remainingCandidate);
+    }
+
+    public function findCandidate_old($currentGroup)
     {
         // look at all teams
         foreach ($this->teams as $team){
@@ -140,7 +287,7 @@ class TeamMgr
 class Team
 {
     /** @var int[] */
-    protected $members;
+    public $members;
 
     /**
      * Team constructor.
